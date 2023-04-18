7aaebcfc2c595592ad1cd3747e0449242cbe31d0
sdttttt <0wk3btfea@mozmail.com>
2023-04-18 10:11:32 +0800

    feat: add sort support.

--- src/views/DirView/index.vue
+++ src/views/DirView/index.vue
@@ -67,6 +67,7 @@ const columns: DataTableColumns<DataItem> = [
     {
         title : "名称",
         key   : "path",
+        sorter: "default",
         render: row => {
             if (!row.size && !row.time) {
                 return h(
@@ -91,15 +92,59 @@ const columns: DataTableColumns<DataItem> = [
 
             return row.path;
         },
+        filterOptions: [
+            {
+                label: "文件夹",
+                value: "dir"
+            },
+            {
+                label: "文件",
+                value: "file"
+            }
+        ],
+        filter: (value, row) => {
+            if ("dir" === value) {
+                return !row.size && !row.time;
+            }
+            if ("file" === value) {
+                return !(!row.size && !row.time);
+            }
+
+            return true;
+        }
     },
     {
-        title: "大小",
-        key  : "size",
+        title : "大小",
+        key   : "size",
+        sorter: (row1, row2) => {
+            row1.size = row1.size || "";
+            row2.size = row2.size || "";
+            const size1 = Number(row1.size?.split("").filter(t => /\d/g.test(t)).join(""));
+            const size2 = Number(row2.size?.split("").filter(t => /\d/g.test(t)).join(""));
+            if (!size1) {
+                return -1;
+            }
+
+            return size1 - size2;
+        },
     },
     {
-        title: "修改时间",
-        key  : "time",
+        title : "修改时间",
+        key   : "time",
+        sorter: (row1, row2) => {
+            row1.time = row1.time || "";
+            row2.time = row2.time || "";
+            if ("" === row1.time) {
+                return -1;
+            }
+
+            const time1 = new Date(row1.time);
+            const time2 = new Date(row2.time);
+
+            return time1.valueOf() - time2.valueOf();
+        },
     },
+
 ];
 
 const {
